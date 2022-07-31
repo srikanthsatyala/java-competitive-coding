3c1b5b098fb50a78d4298ea6c94d8ed5442dfa22
srikanthsatyala <62135728+srikanthsatyala@users.noreply.github.com>
2022-07-31 12:22:53 +0530

    Create 35. Search Insert Position.java

---
+++ Searching/35. Search Insert Position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+       int low=0, high=nums.length-1;
+        while(low<=high){
+            int mid=low+high >> 1;
+            if(target>nums[mid]){
+                low = mid+1;
+            }
+            else if(target<nums[mid])
+                high=mid-1;
+            else if(target==nums[mid])
+                return mid;
+        }
+        return low; 
+    }
+}
